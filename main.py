81daf4dc42b26b8fc2495138cb81bcb20f1cecea
ChampagneM12 <61005552+ChampagneM12@users.noreply.github.com>
2021-04-12 21:41:31 +0200

    first comit

---
+++ main.py
@@ -0,0 +1,79 @@
+import discord
+from discord.ext import commands
+import requests
+
+bot = commands.Bot(command_prefix ="!", descripton = "Bot hennymhome")
+
+
+
+@bot.event
+async def on_ready():
+	print("ready !")
+	
+@bot.command()
+async def talk(ctx, args):
+	await ctx.send(args)
+	
+@bot.command()
+async def manual():
+	await ctx.send(args)
+
+
+@bot.command()
+async def d(ctx, *, args=''):
+	url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="+args
+	R = requests.get(url)
+	if R.status_code !=200:
+		await ctx.send("GET method returned:", status_code)
+	elif R is None:
+		await ctx.send("response is null")
+	else :
+		drinks=R.json()['drinks']
+		if drinks != None:
+			for items in drinks:
+				await ctx.send(items['strDrink'])
+		else :
+			await ctx.send("The drink you are looking for "+args+" is very rare, I can't find it!")
+			
+			
+	
+@bot.command()
+async def c(ctx, *, args=''):
+	url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="+args
+	R = requests.get(url)
+	if R.status_code !=200:
+		await ctx.send("GET method returned:", status_code)
+	elif R is None:
+		await ctx.send("response is null")
+	else :
+		drinks=R.json()['drinks']
+		if drinks != None:
+			for items in drinks:
+				if items['strDrink'].lower() == args.lower():
+					await ctx.send(items['strDrink']+ " " + items['strDrinkThumb'])
+		else :
+			await ctx.send("The drink you are looking for "+args+" is very rare, I can't find it!")	
+			
+@bot.command()
+async def r(ctx, *, args=''):
+	url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="+args
+	R = requests.get(url)
+	if R.status_code !=200:
+		await ctx.send("GET method returned:", status_code)
+	elif R is None:
+		await ctx.send("response is null")
+	else :
+		drinks=R.json()['drinks']
+		if drinks != None:
+			for items in drinks:
+				print(args, items['strDrink'])
+				if items['strDrink'].lower() == args.lower():
+					await ctx.send(items['strInstructions'])
+		else :
+			await ctx.send("The drink you are looking for "+args+" is very rare, I can't find it!")
+			
+			
+
+	
+
+bot.run("ODMwODA4NTI0MTg2OTc2Mjc2.YHMEuw.20nFSRkl7Wux2Zwb7gm2PS6RKc8")
